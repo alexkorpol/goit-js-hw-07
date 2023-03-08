fadccbf02b7e18112381be5b42c5f69bf5a2c07c
Alexander <alexkorpol1964@gmail.com>
2023-03-09 00:21:54 +0200

    Continue correct Task#1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -22,17 +22,42 @@ const clickOnGallery = (event) => {
   
   const selectImg = event.target;
   selectImg.src = selectImg.dataset.source;
+  if (!selectImg.src) return;
   const instance = basicLightbox.create(`
 		<img width="800" height="600" src=${selectImg.src}>
 	`);
   instance.show();
 
-document.addEventListener("keydown", event => {
-  if (event.code === "Escape") {
-    instance.close()
-  }
-});
-};
+  document.addEventListener("keydown", event => {
+    if (event.code === "Escape") {
+      instance.close();
+      console.log("press Escape");
+    };
+    document.removeEventListener("keydown", event => {
+    if (event.code === "Escape") {
+      instance.close();
+      console.log("unpress Escape");
+    }
+  }, false)
+  }, false);
+  
+}
+// const pressEsc = (event) => {
+//   if (event.code === "Escape") {
+//     instance.close();
+//     console.log("press Escape");
+//     document.removeEventListener("keydown", pressEsc);
+//   }
+// }
+
+
+// function pressEsc(event) {
+//   if (event.code === "Escape") {
+//     return this.instance.close()
+//   }
+//   }
+//   };
   
 gallery.addEventListener("click", clickOnGallery);
 console.log(galleryItems);
+
